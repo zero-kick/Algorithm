4c4fe725a265ed0fdfa17ace2c394d1b88122e36
zerokick <88218912+zero-kick@users.noreply.github.com>
2023-04-03 21:29:04 +0900

    Update 문자열을 정수로 바꾸기.java

--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
@@ -1,8 +1,37 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        int solCase = 1;
         
-        answer = Integer.parseInt(s);
+        switch(solCase) {
+            case 1:
+                answer = Integer.parseInt(s);
+                break;
+            default:
+                String mark = "";
+        
+                for(int i = 1; i <= s.length(); i++) {
+                    char ch = s.charAt(i-1);
+
+                    if(i == 1) {
+                        if(ch == '-') {
+                            mark = "minus";
+                        } else if(ch == '+') {
+                            mark = "plus";
+                        } else {
+                            mark = "plus";
+                            answer += (Character.getNumericValue(ch) * Math.pow(10, s.length() - i));
+                        }
+
+                        continue;
+                    }
+
+                    answer += (Character.getNumericValue(ch) * Math.pow(10, s.length() - i));
+                }
+
+                answer = mark.equals("plus") ? answer : answer * (-1);
+                break;
+        }
         
         return answer;
     }
