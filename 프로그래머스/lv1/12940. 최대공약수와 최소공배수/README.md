e7a506543c054f61ce15e2e89d7a097a4c890d43
zerokick <88218912+zero-kick@users.noreply.github.com>
2023-04-15 09:35:33 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/README.md
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최대공약수와 최소공배수 - 12940 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940) 
 
 ### 성능 요약
 
-메모리: 75.1 MB, 시간: 0.12 ms
+메모리: 75.8 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,6 +1,3 @@
-import java.util.List;
-import java.util.ArrayList;
-
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
@@ -8,54 +5,16 @@ class Solution {
         int max = Math.max(n, m);
         int min = Math.min(n, m);
         
-        List<Integer> maxDivisorList = new ArrayList<Integer>();
-        List<Integer> minDivisorList = new ArrayList<Integer>();
-       
-        // 각각의 약수 구하기
-        getSubMultiple(maxDivisorList, max);
-        getSubMultiple(minDivisorList, min);
-        
-        // 최대공약수 구하기
-        int gcd = 0;
-        
-        for(int i = 0; i < maxDivisorList.size(); i++) {
-            for(int j = 0; j < minDivisorList.size(); j++) {
-                if(maxDivisorList.get(i)%minDivisorList.get(j)==0) {
-                    gcd = minDivisorList.get(j);
-                    break;
-                }
-            }
-            if(gcd != 0) break;
-        }
-        
-        // 최소공배수 구하기
-        boolean flag = true;
-        int lcm = 0;
-        int val = max;
-        int i = 1;
-        
-        while(flag) {
-            if(max%min==0) {
-                lcm = max;
-                flag = false;
-            } else {
-                i++;
-                max = val * i;
-            }
-            
-        }
-        
-        answer[0] = gcd;
-        answer[1] = lcm;
+        answer[0] = getGcd(max, min);
+        answer[1] = max * min / answer[0];
         
         return answer;
     }
     
-    public void getSubMultiple (List<Integer> DivisorList, int val) {
-        for(int i = val; i > 0; i--) {
-            if(val%i==0) {
-                DivisorList.add(i);
-            }
-        }
+    public int getGcd(int max, int min) {
+
+        if(max%min == 0) return min;
+        else return getGcd(min, max%min);
+
     }
 }
