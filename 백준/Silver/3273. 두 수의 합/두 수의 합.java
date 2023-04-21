fb93f9527b7f6b2e0acafc8b15a6cd81fe283aa4
zerokick <88218912+zero-kick@users.noreply.github.com>
2023-04-21 21:20:40 +0900

    [Silver III] Title: 두 수의 합, Time: 288 ms, Memory: 26440 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,30 +1,31 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        String[] arr = br.readLine().split(" ");
-        int x = Integer.parseInt(br.readLine());
-
-        Arrays.sort(arr);
-
-        int findVal = 0;
-        int idx = 0;
         int answer = 0;
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        boolean[] boolArr = new boolean[2000001];
         for(int i = 0; i < n; i++) {
-            findVal = x - Integer.parseInt(arr[i]);
-            arr[i] = "";
-            idx = Arrays.binarySearch(arr, String.valueOf(findVal));
-            if(idx >= 0) {
+            boolArr[Integer.parseInt(st.nextToken())] = true;
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        for(int i = 1; i <= x; i++) {
+            if(boolArr[i] && boolArr[x-i]) {
                 answer++;
             }
         }
 
-        System.out.println(answer);
+        System.out.println(answer/2);
     }
 }
