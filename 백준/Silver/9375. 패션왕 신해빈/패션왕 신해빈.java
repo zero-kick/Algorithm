85c314d73a36d1ac5742b1572c8bc11af95469f2
zerokick <88218912+zero-kick@users.noreply.github.com>
2024-09-18 00:36:20 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 108 ms, Memory: 14476 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int tc, n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        tc = Integer.parseInt(br.readLine());
+        for(int i = 0; i < tc; i++) {
+            n = Integer.parseInt(br.readLine());
+            HashMap<String, Integer> map = new HashMap<>();
+            for(int j = 0; j < n; j++) {
+                st = new StringTokenizer(br.readLine());
+                String name = st.nextToken();
+                String kind = st.nextToken();
+                map.put(kind, map.getOrDefault(kind, 0)+1);
+            }
+
+            int[] arr = map.values().stream().mapToInt(a -> a.intValue()).toArray();
+
+            int cnt = 1;
+            for(int a : arr) {
+                cnt = cnt * (a+1);
+            }
+            answer.add(cnt-1);
+        }
+
+        for(int a : answer)
+            bw.write(String.valueOf(a) + "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
