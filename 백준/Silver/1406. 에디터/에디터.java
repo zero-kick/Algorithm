74e7cbcb5bedab4b54c1fd343f2cd5030c752459
zerokick <88218912+zero-kick@users.noreply.github.com>
2024-09-25 21:40:50 +0900

    [Silver II] Title: 에디터, Time: 656 ms, Memory: 140300 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -6,50 +6,59 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        String answer = "";
+        Stack<Character> stkLeft = new Stack<>();
+        Stack<Character> stkRight = new Stack<>();
 
-        String str = br.readLine();
-        Stack<Character> leftStk = new Stack<Character>();
-        Stack<Character> rightStk = new Stack<Character>();
+        String string = br.readLine();
+        int len = string.length();
 
-        for(int i = 0; i < str.length(); i++) {
-            leftStk.push(str.charAt(i));
-        }
+        for (int i = 0; i < len; i++)
+            stkLeft.push(string.charAt(i));
 
         int m = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            String order = st.nextToken();
 
-            switch(st.nextToken()) {
-                case "L":
-                    if(leftStk.isEmpty()) break;
-                    rightStk.push(leftStk.pop());
+            switch (order) {
+                case "L" :
+                    if (stkLeft.isEmpty())
+                        continue;
+                    else
+                        stkRight.push(stkLeft.pop());
                     break;
-                case "D":
-                    if(rightStk.isEmpty()) break;
-                    leftStk.push(rightStk.pop());
+                case "D" :
+                    if (stkRight.isEmpty())
+                        continue;
+                    else
+                        stkLeft.push(stkRight.pop());
                     break;
-                case "B":
-                    if(leftStk.isEmpty()) break;
-                    leftStk.pop();
+                case "B" :
+                    if (stkLeft.isEmpty())
+                        continue;
+                    else
+                        stkLeft.pop();
                     break;
-                case "P":
-                    leftStk.push(st.nextToken().charAt(0));
-                    break;
+                case "P" :
+                    String s = st.nextToken();
+                    stkLeft.push(s.charAt(0));
             }
         }
 
-        while(!leftStk.isEmpty()) {
-            rightStk.push(leftStk.pop());
-        }
-        while(!rightStk.isEmpty()) {
-            bw.write(rightStk.pop());
-        }
+        while (!stkRight.isEmpty())
+            stkLeft.push(stkRight.pop());
 
-        br.close();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < stkLeft.size(); i++)
+            sb.append(stkLeft.get(i));
+
+        bw.write(sb.toString());
+
         bw.flush();
         bw.close();
+        br.close();
     }
 }
