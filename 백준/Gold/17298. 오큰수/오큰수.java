a08aedbf281ed7777fcda40205658d60ef243228
zerokick <88218912+zero-kick@users.noreply.github.com>
2024-09-27 16:16:50 +0900

    [Gold IV] Title: 오큰수, Time: 888 ms, Memory: 150184 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,63 +1,38 @@
 import java.io.*;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static int n;
-    public static int[] a;
-    public static int[] nge;
-    public static Stack<Integer> stk;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        n = Integer.parseInt(br.readLine());
-        a = new int[n];
-        nge = new int[n];
-        stk = new Stack<Integer>();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] answer = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+        Stack<Integer> stk = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            while (!stk.isEmpty() && arr[stk.peek()] < arr[i])
+                answer[stk.pop()] = arr[i];
+
+            stk.push(i);
         }
 
-        // 오큰수 찾기
-        findNge();
+        while (!stk.isEmpty())
+            answer[stk.pop()] = -1;
 
-        // 정답 출력
-        for(int i = 0; i < nge.length; i++) {
-            bw.write(String.valueOf(nge[i]) + " ");
-        }
+        for (int a : answer)
+            bw.write(a + " ");
 
         bw.flush();
         bw.close();
         br.close();
     }
 
-    public static void findNge() {
-        // n개의 수를 돌면서
-        for(int i = 0; i < n; i++) {
-            // 스택에 a배열의 인덱스가 들어있는 경우
-            while(!stk.isEmpty()) {
-                // 현재 값과 스택에 들어있는 인덱스를 갖는 배열 값을 비교하여
-                // 배열의 값이 큰 경우 오큰수이므로, nge[스택pop인덱스]에 해당 값을 할당한다.
-                if(a[stk.peek()] < a[i]) {
-                    nge[stk.pop()] = a[i];
-                // 현재 값보다 스택에 들어있는 인덱스를 갖는 배열의 값이 더 크거나 같은 경우
-                // 오큰수가 아니므로 현재 값을 스택에 담아주고 다음 인덱스를 탐색한다.
-                } else {
-                    break;
-                }
-            }
-            stk.push(i);
-        }
-
-        // 수열을 모두 탐색하였는데도 스택에 남아있는 수의 경우
-        // 오큰수가 존재하지 않는다는 것이므로 -1을 할당한다.
-        while(!stk.isEmpty()) {
-            nge[stk.pop()] = -1;
-        }
-    }
 }
