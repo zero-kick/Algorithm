bfed56f942b4792e854f7a94e41d4a033cb7b1da
zerokick <88218912+zero-kick@users.noreply.github.com>
2024-11-07 17:14:56 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+
+        if (n == 1) bw.write("1");
+        else {
+            int[] count = new int[n+1];
+            count[1] = 1;
+            count[2] = 2;
+
+            for (int i = 3; i <= n; i++)
+                count[i] = (count[i-1] + count[i-2]) % 10007;
+
+            bw.write(String.valueOf(count[n]));
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
